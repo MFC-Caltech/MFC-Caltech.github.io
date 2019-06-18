9d1ef0805cec5c48e3358dfd7ddf50cd1ef4054c
Spencer Bryngelson <sbryngelson@gmail.com>
2019-06-17 17:26:29 -0700

    update

---
+++ simulation/navtreedata.js
@@ -0,0 +1,58 @@
+var NAVTREE =
+[
+  [ "MFC:Simulation", "index.html", [
+    [ "MFC documentation", "index.html", [
+      [ "Organization", "index.html#org", null ],
+      [ "Introduction", "index.html#intro_sec", null ],
+      [ "Installation", "index.html#install_sec", [
+        [ "Step 1: Configure and ensure dependencies can be located", "index.html#step1", null ],
+        [ "Step 2: Install/Build", "index.html#step2", null ]
+      ] ],
+      [ "License", "index.html#license_sec", null ],
+      [ "Useful documents", "index.html#documents_sec", [
+        [ "User's guide", "index.html#user", null ],
+        [ "MFC paper", "index.html#paper", null ],
+        [ "Related publications", "index.html#pub", null ]
+      ] ]
+    ] ],
+    [ "Pre_process", "^https://bryngelson-research.com/pre_process/index.html", null ],
+    [ "Simulation", "^https://bryngelson-research.com/simulation/index.html", null ],
+    [ "Post_process", "^https://bryngelson-research.com/post_process/index.html", null ],
+    [ "Modules", null, [
+      [ "Modules List", "namespaces.html", "namespaces" ],
+      [ "Module Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions/Subroutines", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", "namespacemembers_vars" ]
+      ] ]
+    ] ],
+    [ "Data Types List", "annotated.html", [
+      [ "Data Types List", "annotated.html", "annotated_dup" ],
+      [ "Data Types", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions/Subroutines", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions/Subroutines", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"m__global__parameters_8f90.html#a5422a3820409740b5c3b0b2e88bdb2b0",
+"m__rhs_8f90.html#aeadbc0ce9b66517f8fde156199772ec1",
+"m__variables__conversion_8f90.html#abe0d038aac3b3f0166c1a0c93c497e33"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
