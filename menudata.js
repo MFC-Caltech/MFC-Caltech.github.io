1ebedd0f14eddcb2c2eec70733a73d6aaf6ade2d
Spencer Bryngelson <sbryngelson@gmail.com>
2019-06-20 08:52:33 -0700

    asdf

--- menudata.js
+++
@@ -1,4 +0,0 @@
-var menudata={children:[
-{text:"Pre_process",url:"https://mfc-caltech.github.io/pre_process/index.html"},
-{text:"Simulation",url:"https://mfc-caltech.github.io/simulation/index.html"},
-{text:"Post_process",url:"https://mfc-caltech.github.io/post_process/index.html"}]}
